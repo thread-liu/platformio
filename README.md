9c40317f508c9e2c1604ad4178ad87979d4c5983
thread-liu <lk9608@outlook.com>
2020-11-25 09:08:25 +0800

    [update] platformio.

--- README.md
+++ README.md
@@ -1,46 +1,86 @@
-# platformio_core_for_studio
+# PlatformIO CLI
 
->一个可发布的 platformio 最小包
+- 自动拉取 Platformio-core；
+- 自动检查 Plafomio-core 是否更新；
+- 自动更新 platformio-core；
+- 自动测试 platformio 功能， 进行工程的创建，编译测试；
+- 自动打包。
 
+## boards list：
 
+```shell
+platformio boards --json-output
+```
 
-## 1. 支持的平台：
+- --json-output: 以 json 格式输出
 
-+ win10 x64
+  ```shell
+      {
+          "id": "nucleo_f072rb",
+          "name": "ST Nucleo F072RB",
+          "platform": "ststm32",
+          "mcu": "STM32F072RBT6",
+          "fcpu": 48000000,
+          "ram": 16384,
+          "rom": 131072,
+          "frameworks": [
+              "arduino",
+              "cmsis",
+              "mbed",
+              "stm32cube",
+              "libopencm3"
+          ],
+          "vendor": "ST",
+          "url": "https://developer.mbed.org/platforms/ST-Nucleo-F072RB/",
+          "connectivity": [
+              "can"
+          ],
+          "debug": {
+              "tools": {
+                  "blackmagic": {},
+                  "jlink": {},
+                  "stlink": {
+                      "onboard": true,
+                      "default": true
+                  }
+              }
+          }
+      },
+  ```
 
-## 2. 使用方法
+## Init Project：
 
-1. 下载仓库到本地目录
+```shell
+platformio init --board nucleo_h743zi --ide=eclipse --project-option framework=cmsis --project-dir test
+```
 
-2. （可选）添加仓库根目录到环境变量Path ，这样可以全局使用platformio 命令
+- **-b, --board** : board ID
+- **--ide** ：IDE
+- **-O, --project-option** ： custom `platformio.ini` file
+- **-d, --project-dir** ：project path
 
-   如果不执行此步骤，则需要使用据对路径调用
+## Build  Project
 
-3. 根据 platformio core 的[文档](https://docs.platformio.org/en/latest/core/quickstart.html)使用 platformio
+```shell
+platformio -f -c eclipse run
+```
 
-## 3. 发布步骤
+## Clean  Project
 
-1. 克隆仓库到本地
-2. 删除piodebuggdb.py文件和platformio.py 文件
-3. 将其他文件打包到 studio 安装包
+```shell
+platformio -f -c eclipse run --target clean
+```
 
-## 4. 使用 Python 打包的步骤
+## Upload  Project
 
-1. 克隆仓库到本地
-2. platformio 代码在platform_core/Lib 下，如果需要更新platformio，需要更新这里的库
-3. 根据需要调整入口文件piodebuggdb.py文件和platformio.py
-4. 执行 `pyinstaller -F  platformio.py` 打包生成 platformio.exe
-5. 执行 `pyinstaller -F  piodebuggdb.py` 打包生成 piodebuggdb.exe
+```shell
+platformio -f -c eclipse run --target upload
+```
 
-## 5. 使用 golang 打包的步骤
+## Debug Project
 
-1. 克隆仓库到本地
-2. platformio 代码在platform_core/Lib 下，如果需要更新platformio，需要更新这里的库
-3. 根据需要调整入口文件 `golang_entrance\piodebuggdb\piodebuggdb.go` 文件和`golang_entrance\platformio\platformio.go`
-4. 在 golang_entrance 目录下执行 `go build platformio\platformio.go` 打包生成 platformio.exe
-5. 在 golang_entrance 目录下执行 `go build piodebuggdb\piodebuggdb.go` 打包生成 piodebuggdb.exe
-6. 将生成的 exe 和 platformio_core 文件夹放在同级目录下运行
+```shell
+platformio -f -c eclipse debug --interface=gdb -x .pioinit
+```
 
-## 6. platformio 应用示例
-
-​	参考[文档](https://git.rt-thread.com/realthread/ide_bug_report/uploads/5494f65d2f8358a480bd856890137006/%E5%88%9D%E6%AD%A5%E6%96%B9%E6%A1%881110.pdf)
+- **--interface=gdb** : start GDB 
--- release.py
+++ release.py
@@ -1,8 +1,9 @@
 import os
 import sys
+import json
 import shutil
 import logging
-
+import tarfile
 
 
 def init_logger():
@@ -12,6 +13,16 @@ def init_logger():
                         format=log_format,
                         datefmt=date_format
                         )
+
+
+def do_remove_folder(dst_dir):
+    try:
+        if os.path.exists(dst_dir):
+            shutil.rmtree(dst_dir)
+    except Exception as e:
+        logging.error("Error message : {0}.".format(e))
+        raise Exception("ERROR: Can‘t delete folder {0}, please check the resource.".format(dst_dir))
+    return True
 
 
 def do_copy_file(src, dst):
@@ -31,57 +42,132 @@ def do_copy_file(src, dst):
     return True
 
 
+def do_copy_folder(src_dir, dst_dir, ignore=None):
+    if not os.path.exists(src_dir):
+        raise Exception("ERROR: Can’t get resource {0}, please check the resource.".format(src_dir))
+
+    try:
+        shutil.copytree(src_dir, dst_dir, ignore=ignore)
+    except Exception as e:
+        logging.error("Error message: {0}".format(e))
+        raise Exception("ERROR: Copy {0} failed.".format(src_dir))
+
+    return True
+
+
 def platformio_test(cwd):
-    os.mkdir(os.path.join(cwd, "test"))
     do_copy_file(os.path.join(cwd, "dist", "platformio.exe"), os.path.join(cwd, "platformio.exe"))
     do_copy_file(os.path.join(cwd, "dist", "pio.exe"), os.path.join(cwd, "pio.exe"))
     do_copy_file(os.path.join(cwd, "dist", "piodebuggdb.exe"), os.path.join(cwd,"piodebuggdb.exe"))
+
+    if os.system("pio --version") != 0:
+        sys.exit(1)
+
+    logging.info("list all platformio boards :")
     os.system("pio boards")
-    os.system("platformio boards")
-    os.system("platformio init --board nucleo_h743zi --ide=eclipse --project-option framework=cmsis --project-dir test")
-    os.system("platformio -f -c eclipse run --project-dir test")
-    os.system("platformio -f -c eclipse run --target clean --project-dir test")
-    
+
+    logging.info("create a arduino project, works at Eclipse IDE :")
+    os.mkdir(os.path.join(cwd, "prj_nucleo_h743zi"))
+    os.system("platformio init --board nucleo_h743zi --ide=eclipse --project-option framework=cmsis --project-dir prj_nucleo_h743zi")
+
+    logging.info("build project :")
+    os.system("platformio -f -c eclipse run --project-dir prj_nucleo_h743zi")
+
+    logging.info("clean project :")
+    os.system("platformio -f -c eclipse run --target clean --project-dir prj_nucleo_h743zi")
+
     return 0
 
-def win_release_to_oss():
-    try:
-        if os.system(r'ossutil64 -c %FILE_OSSUTILCONFIG% cp -r -f release_platformio oss://realthread-download/rt-studio/backend/') != 0:
-            raise Exception('Deploy to oss fail.')
-    except OSError:
-        raise Exception('Deploy command executes fail.')
+
+def make_targz(output_filename, source_dir):
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
 
 
 def release():
+    """ release platformio core"""
     try:
-        os.system(r'pyinstaller --clean --win-private-assemblies  -F ./platformio.py -i ./platformio-logo.ico  --hiddenimport xmltodict')
-        os.system(r'pyinstaller --clean --win-private-assemblies  -F ./pio.py -i ./platformio-logo.ico  --hiddenimport xmltodict')
-        os.system(r'pyinstaller --clean --win-private-assemblies  -F ./piodebuggdb.py -i ./platformio-logo.ico  --hiddenimport xmltodict')
-        os_type = sys.platform
-        logging.info("os type : {0}".format(os_type))
+        os.system(r'pyinstaller --clean --win-private-assemblies  '
+                  r'-F ./platformio.py -i ./platformio-logo.ico  --hiddenimport xmltodict')
+        os.system(r'pyinstaller --clean --win-private-assemblies  '
+                  r'-F ./pio.py -i ./platformio-logo.ico  --hiddenimport xmltodict')
+        os.system(r'pyinstaller --clean --win-private-assemblies  '
+                  r'-F ./piodebuggdb.py -i ./platformio-logo.ico  --hiddenimport xmltodict')
+        logging.info("os type : {0}".format(sys.platform))
         cwd = os.getcwd()
-        if os_type.find("linux") != -1:
-            pass
-        else:
-            do_copy_file(os.path.join(cwd, "dist", "platformio.exe"), os.path.join(cwd, "release_platformio", "platformio.exe"))
-            do_copy_file(os.path.join(cwd, "dist", "pio.exe"), os.path.join(cwd, "release_platformio", "pio.exe"))
-            do_copy_file(os.path.join(cwd, "dist", "piodebuggdb.exe"), os.path.join(cwd, "release_platformio", "piodebuggdb.exe"))
+
+        do_copy_file(os.path.join(cwd, "dist", "platformio.exe"),
+                     os.path.join(cwd, "release_platformio", "platformio.exe"))
+        do_copy_file(os.path.join(cwd, "dist", "pio.exe"),
+                     os.path.join(cwd, "release_platformio", "pio.exe"))
+        do_copy_file(os.path.join(cwd, "dist", "piodebuggdb.exe"),
+                     os.path.join(cwd, "release_platformio", "piodebuggdb.exe"))
+        do_copy_folder(os.path.join(cwd, "platformio_core"),
+                       os.path.join(cwd, "release_platformio", "platformio_core"))
 
         logging.info(os.listdir(os.path.join(cwd, "release_platformio")))
+        do_remove_folder(os.path.join(cwd, "platformio_core", "Scripts"))
 
     except Exception as e:
         logging.error("Error message : {0}".format(e))
+        sys.exit(1)
 
     platformio_test(cwd)
-    win_release_to_oss()
 
     return 0
 
+
+def update_platformio_core(json_path):
+    try:
+        with open(json_path, 'r') as f:
+            json_data = json.load(f)
+    except Exception as e:
+        logging.error(e)
+        sys.exit(1)
+
+    if len(json_data) == 0:
+        return 0
+
+    latest_version = None
+    old_version = None
+    for package in json_data:
+        for key, value in package.items():
+            if (key == "name") and (value == "platformio"):
+                latest_version = package['latest_version']
+                old_version = package['version']
+                break
+
+    logging.info("platformio will upgrade {0} ==> {1}".format(old_version, latest_version))
+    cmd = "python.exe -m pip install -U platformio=={0} --no-warn-script-location".format(latest_version)
+    logging.info(cmd)
+    os.system(cmd)
+
+
+def install_platformio_core():
+    """ update platformio version """
+    old_path = os.getcwd()
+    os.chdir(os.path.join(old_path, "platformio_core"))
+
+    if os.system("python.exe -m pip install platformio --no-warn-script-location") != 0:
+        sys.exit(1)
+    os.system("python.exe -m pip list")
+
+    os.system("python.exe -m pip list --outdated --format=json > py_pip.json")
+    json_path = os.path.join(old_path, "platformio_core", "py_pip.json")
+    update_platformio_core(json_path)
+
+    os.system("python.exe -m pip list")
+    os.chdir(old_path)
+
+    return 0
 
 
 def main():
     init_logger()
+    install_platformio_core()
     release()
+
+    return 0
 
 
 if __name__ == "__main__":
